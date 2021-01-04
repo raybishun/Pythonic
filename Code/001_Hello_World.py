6dc97dbe6cd9aafb9c845d9c67bcde8036c16273
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2021-01-04 18:44:50 -0500

    Python does NOT support Block Comments, and is NOT strongly typed

---
+++ Code/001_Hello_World.py
@@ -0,0 +1,18 @@
+"""
+- This is a multi-line string object!!!
+- Not a comment block!!!
+"""
+
+# Use of single (')
+greeting1 = 'Hello, World!'
+
+# Use of quotes (")
+greeting2 = "Hello, World!"
+
+# Python is not strongly typed
+greeting1 = 1.618
+
+print(greeting1)
+print(greeting2)
+
+print(greeting1)
