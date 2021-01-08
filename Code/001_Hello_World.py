eb98f18127aa134b39a743ddc9e283656bdccb3c
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2021-01-07 19:41:04 -0500

    Numeric Operators

--- Code/001_Hello_World.py
+++ Code/001_Hello_World.py
@@ -29,3 +29,28 @@ print(index)
 scientific_notation = 4.5e9
 x = 4.5 * (10 ** 9)
 print(scientific_notation == x)
+
+# Normal Division
+print( 5 / 3 )
+
+# Floor Division
+# Return the number of times 5 'evenly' divides into 3s, as an int
+print ( 5 // 3 ) 
+
+# Modulo
+# Return the remainder as an int
+# Common usage for modulo is to identify odd or even numbers
+print ( 5 % 3 )
+
+# Exponents
+print ( 2 ** 3 ) # 2 x 2 x 2
+
+# Number Systems
+# -----------------------------------------------------------------------------
+# Decimal is base 10    0 - 9
+# Binary is base 2      0 or 1
+
+# Number Systems: Examples
+# -----------------------------------------------------------------------------
+# Decimal 15 is 1111 in Binary
+print(  )
