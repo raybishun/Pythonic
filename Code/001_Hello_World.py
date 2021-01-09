783b294753a479ad9d7be1139266c18b9e551834
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2021-01-08 23:02:57 -0500

    Number Systems...

--- Code/001_Hello_World.py
+++ Code/001_Hello_World.py
@@ -47,10 +47,48 @@ print ( 2 ** 3 ) # 2 x 2 x 2
 
 # Number Systems
 # -----------------------------------------------------------------------------
-# Decimal is base 10    0 - 9
-# Binary is base 2      0 or 1
+# Decimal       is  Base 10     0 - 9
+# Binary        is  Base 2      0 or 1
+# Octal         is  Base 8      0 - 7
+# Hexadecimal   is  Base 16     0 - F (0 - 9 and A - F)
 
-# Number Systems: Examples
+# Representing Number Systems
 # -----------------------------------------------------------------------------
-# Decimal 15 is 1111 in Binary
-print(  )
+
+# Prefix: 0b for Binary
+print(0b101)
+
+# Prefix: 0o for Octal
+print(0o7242)
+
+# Prefix: 0x for Hexadecimal
+print(0xFF012)
+
+# Converting Decimal to Binary
+# -----------------------------------------------------------------------------
+# Divide the Decimal by the number system's base that we want to covert to, i.e.
+myDecimal_Number = 15
+myBase = 2
+
+result1 = myDecimal_Number % myBase
+print(f'result1: {result1}') # remainder is 1
+
+result2 = result1 % myBase
+print(f'result2: {result2}')  # remainder is 1
+
+result3 = result2 % myBase
+print(f'result3: {result3}')  # remainder is 1
+
+result4 = result3 % myBase
+print(f'result4: {result4}')  # remainder is 1
+
+# The remainders from Least Significant to Most Significant
+print(f'{result1}{result2}{result3}{result4}') # returns 1111
+
+
+# Converting from Binary, back to Decimal (where the binary value is 1111)
+# -----------------------------------------------------------------------------
+# Most Significant .......... Least Significant
+#  (1 * 2^3)  +  (1 * 2^2) + (1 * 2^1) + (1 * 2^0)   where ^ is 'power'
+# (1 * 2*2*2) +  (1 * 2*2) + (1 * 2) + (1)
+#        8    +        4   +      2  +  1 = 15
