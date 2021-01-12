54bc822cbc686da551ad28194bcbc75f8e651f7d
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2021-01-11 20:17:38 -0500

    Two's Complement

--- Code/002_Operators_Bindings.py
+++ Code/002_Operators_Bindings.py
@@ -1,6 +1,5 @@
 # Operators and Bindings
-
-
+# -----------------------------------------------------------------------------
 # Get int from binary 
 a = 0b010
 print(a) # returns 2
@@ -9,11 +8,19 @@ print(a) # returns 2
 print(bin(a)) # returns 0b10
 
 # Bitwise Complement operator 
-# Converts over to Two's complement of our binary representation
-# Two's complement is a way to represent negative numbers, and always begins with a 1
+# -----------------------------------------------------------------------------
+# Converts to Two's complement of a binary number
+# Two's complement is a way to represent negative numbers, 
+# and they always begins with a 1
 print (~a) # retuns -3
 
 # The formula is: your_number * -1 subtract 1
-print(a * -1 - 1)
+print(a * -1 - 1) # retuns -3
 
+# Or simply
+print(-a -1) # retuns -3
+
+# Binary representation of Complement 'a'
 print(bin(~a)) # returns -0b11
+
+# Truth Tables
