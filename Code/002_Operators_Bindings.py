092bd71b1d9cbac8c506f52d698fc31a952044ed
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2021-01-13 20:22:38 -0500

    XOR and Shift-Operators

--- Code/002_Operators_Bindings.py
+++ Code/002_Operators_Bindings.py
@@ -42,3 +42,27 @@ print(bin(a))
 print(bin(b))
 print(c)
 print()
+
+# XOR, aka Exclusive Or (only one must be true)
+c = bin(a ^ b)
+print(bin(a))
+print(bin(b))
+print(f' {c}')
+print()
+
+# Shift Operators (change the binary representation by moving right or left)
+# -----------------------------------------------------------------------------
+a = 0b110
+print(a) # returns 6 (4 + 2)
+print()
+
+# Right-Shift
+print(a >> 2) # returns 1
+print()
+
+print(a >> 4) # NOTE: returns 0 if you shift more than the number you have
+print()
+
+# Left-Shift (padds with leading 0s)
+print(a << 2) # returns 24
+print()
