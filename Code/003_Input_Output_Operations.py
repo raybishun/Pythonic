2eaa0b6f8659277ca77f2789a67b5995d8f95ff8
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2021-01-18 15:34:08 -0500

    Input with type casting

--- Code/003_Input_Output_Operations.py
+++ Code/003_Input_Output_Operations.py
@@ -11,4 +11,13 @@ print(int('1'))
 
 # Interesting this error is returned:
 # --> Exception has occurred: ValueError: invalid literal for int() with base 10: '1.618'
-print(int('1.618')) 
+# print(int('1.618')) 
+
+print()
+
+# Input (with type casting)
+# -----------------------------------------------------------------------------
+fname = str(input("Enter first name: "))
+lname = str(input('Enter last name: '))
+age = int(input('Age: '))
+print(f'{lname}, ' f'{fname}', f'{age}')
