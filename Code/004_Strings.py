68763d654cce4fe1d50ffb7597347ad3d41b294f
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2021-01-20 21:58:46 -0500

    The Immutability of Strings...

---
+++ Code/004_Strings.py
@@ -0,0 +1,24 @@
+# Strings
+# -----------------------------------------------------------------------------
+myStr = 'my string'
+print(f'{myStr}', f'{id(myStr)}')
+
+# this actually creates a NEW string
+myStr = myStr.capitalize() 
+print(f'{myStr}', f'{id(myStr)}')
+
+print()
+
+
+# Immutability of Strings
+# -----------------------------------------------------------------------------
+
+# Here the 2 variables are pointing to the same memory location
+
+myStr = "Ray"
+print(f'{myStr}', f'{id(myStr)}')
+
+myOtherStr = "Ray"
+print(f'{myOtherStr}', f'{id(myOtherStr)}')
+
+print()
