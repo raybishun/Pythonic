c5a09fdfc51be35a6345fc1770994052ada87702
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2021-01-22 22:09:01 -0500

    Indexing and Slicing

--- Code/004_Strings.py
+++ Code/004_Strings.py
@@ -29,3 +29,47 @@ myStr = "Hello World"
 print(len(myStr))
 
 print()
+
+# Indexing
+# -----------------------------------------------------------------------------
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+print(alphabet[2])
+
+lastLetter = alphabet[len(alphabet) - 1]
+print(lastLetter)
+print(alphabet[-1]) # returns z
+print(alphabet[-3]) # returns x
+
+print()
+
+# Slicing
+# -----------------------------------------------------------------------------
+first3 = alphabet[0:3]
+print(first3)
+
+last3 = alphabet[23:26]
+print(last3)
+
+last3a = alphabet[23:len(alphabet)]
+print(last3a)
+
+last3betterChoice = alphabet[23:]
+print(last3betterChoice)
+
+numbers = "123456789"
+even = numbers[1:9:2]
+print(even)
+
+startToFive = numbers[:5:]
+print(startToFive)
+
+two = numbers[1::9]
+print(two)
+
+three = numbers[2::9]
+print(three)
+
+reverseStr = numbers[::-1]
+print(reverseStr)
+
+print()
