5c90e4e6317c821f81add51106f6953757a18f72
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2021-01-26 21:26:41 -0500

    Tuples

---
+++ Code/Tuples.py
@@ -0,0 +1,12 @@
+# Tuples (immutable)
+# =============================================================================
+point = (2.0, 3.0)
+point_3d = point + (4.0,)
+print(point_3d)
+
+x, y, z = point_3d
+print(x)
+print(y)
+print(z)
+
+print()
