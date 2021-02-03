5729f8c9d132212db2738c48e32c49952755562e
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2021-02-02 19:35:27 -0500

    Conditional Statements 01

---
+++ Code/007_ConditionalStatements.py
@@ -0,0 +1,15 @@
+# Contitional Statements
+# -----------------------------------------------------------------------------
+if 'a' < 'b':
+    print('true')
+    # ...
+    # ...
+else:
+    print('false')
+    # ...
+    # ...
+
+if True:
+    print('true')
+elif False:
+    print('true by default, never gets here...')
