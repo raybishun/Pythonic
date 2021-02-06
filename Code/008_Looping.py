2c9421b465952d72da4425f33f3a1b2369f3204a
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2021-02-06 15:17:34 -0500

    For Loops

--- Code/008_Looping.py
+++ Code/008_Looping.py
@@ -1,6 +1,45 @@
-# Looping: While Loop
-# -----------------------------------------------------------------------------
+# While Loop
+# =============================================================================
 i = 0
 while(i < 10):
     i = i + 1
     print(i)
+
+print()
+
+# For Loops
+# =============================================================================
+
+# Looping through a string
+# -----------------------------------------------------------------------------
+symbol = 'Google' 
+for letter in symbol:
+    print(letter)
+
+print()
+
+# Looping through an array
+# -----------------------------------------------------------------------------
+symbols = ['FB', 'AMZN', 'AAPL', 'NFLX', 'GOOG']
+symbols[4] = 'MSFT'
+for symbol in symbols:
+    print(symbol)
+
+print()
+
+# Looping through a tuple
+# -----------------------------------------------------------------------------
+symbols = ('FB', 'AMZN', 'AAPL', 'NFLX', 'GOOG')
+#symbols[4] = 'MSFT' # No can do, since this is a tuple
+for symbol in symbols:
+    print(symbol)
+
+print()
+
+# Looping through a dictionary
+# -----------------------------------------------------------------------------
+symbols = {'FB': 268.10, 'AMZN': 3352.15, 'AAPL': 136.76, 'NFLX': 550.79, 'GOOG': 2098.00}
+for key, value in symbols.items():
+    print(f'{key}\t{value}')
+
+print()
