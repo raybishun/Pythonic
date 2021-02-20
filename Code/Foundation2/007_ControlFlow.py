c26be5a1034f3a9066c00066c5fe47985830b7d6
Ray Bishun <ray.bishun@bitsbytes.com>
2021-02-19 19:35:58 -0500

    Unpacking Dictionaries and Tuples

--- Code/Foundation2/007_ControlFlow.py
+++ Code/Foundation2/007_ControlFlow.py
@@ -44,3 +44,38 @@ while i < 10:
     print(i)
 
 print()
+
+# Unpacking Dictionaries
+# -----------------------------------------------------------------------------
+faang = {'fb': 261.56, 'aapl': 129.87, 'amzn': 3249.90, 'nflx': 540.22, 'goog': 2101.14}
+for k in faang:
+    print(f'{type(k)}\t{k}')
+print()
+
+for k in faang.keys():
+    print(f'{type(k)}\t{k}')
+print()
+
+for v in faang.values():
+    print(f'{type(v)}\t{v}')
+print()
+
+for item in faang.items():
+        print(f'{type(item)}\t{item}')
+print()
+
+for k, v in faang.items():
+    print(f'{k}\t{v}')
+print()
+
+for (k, v) in faang.items():
+    print(f'{k}\t{v}')
+print()
+print()
+
+# Unpacking Tuples
+# -----------------------------------------------------------------------------
+faang = [('fb', 261.56, -7.83), ('aapl', 129.87, 0.16), ('amzn', 3249.90, -78.33), ('nflx', 540.22, -8.00), ('goog', 2101.14, -16.06)]
+for symbol, close, change in faang:
+    print(f'{symbol}\t{close}\t{change}')
+print()
