50f63b39ccf34320018e3d66077f60ea5bdd961c
Ray Bishun <ray.bishun@bitsbytes.com>
2021-03-08 21:41:00 -0500

    os lib

--- Code/Foundation2/013_Working_With_Folders.py
+++ Code/Foundation2/013_Working_With_Folders.py
@@ -1,8 +1,34 @@
 import os
 
+# Traverse folders
 for path, folders, files in os.walk("Temp\Sub1"):
-    print(f'Path: {path}')
-    print(f'Folders: {folders}')
-    print(f'Files: {files}')
+    # print(f'Path: {path}')
+    # print(f'Folders: {folders}')
+    # print(f'Files: {files}')
+    # print()
+    pass
 
-    print()
+# Get environment variables
+print(os.environ.get("HOMEPATH"))
+print(os.path.join(os.environ.get("HOMEPATH"), "Documents"))
+
+# Get filename
+print(os.path.basename("C:\\Users\\ray\\Documents\\log.txt"))
+
+# Get full directory name
+print(os.path.dirname("C:\\Users\\ray\\Documents\\log.txt"))
+
+# Get both the dir name and file name (as a tuple)
+print(os.path.split("C:\\Users\\ray\\Documents\\log.txt"))
+
+# Check if file and folder exists
+print(os.path.exists("C:\\Users\\ray\\Documents\\log.txt"))
+
+# Check if folder exists
+print(os.path.isdir("C:\\Users\\ray\\Documents"))
+
+# Check if file exists
+print(os.path.isfile("C:\\Users\\ray\\Documents\\log.txt"))
+
+# Get file extension by splitting folder and file name (as a tuple)
+print(os.path.splitext("C:\\Users\\ray\\Documents\\log.txt"))
