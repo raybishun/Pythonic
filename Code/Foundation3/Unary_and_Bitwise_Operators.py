3af8821e538e50f2a1709011ec31233ca2abcde3
Ray Bishun <ray.bishun@bitsbytes.com>
2021-03-11 21:31:34 -0500

    Unary and Bitwise Operators

--- Code/Foundation3/Unary_and_Bitwise_Operators.py
+++ Code/Foundation3/Unary_and_Bitwise_Operators.py
@@ -1,5 +1,6 @@
-# Unary Operators
-# -----------------------------------------------------------------------------
+# =============================================================================
+# Unary Operators (with a single operand)
+# =============================================================================
 # Bitwise complement operator (the ~), converts to the 'twos complement' of a binary representation
 # Twos complement is a way of representing a negative number
 # Shortcut is:  complement  a = -1 * a - 1
@@ -7,9 +8,34 @@ a = 0b010
 print(a)        # a = 2
 print(bin(a))   # 0b10
 print(~a)       # -3 (-1 * a - 1)
+print()
 
+# =============================================================================
+# Truth Tables [OR, AND, XOR (exclusive OR), NOT]
+# =============================================================================
 
-# Truth Tables
+# ORing
 # -----------------------------------------------------------------------------
 a = 0b1001      # 9
-print(a)
+b = 0b1100      # 12
+print(bin(a))
+print(bin(b))
+print(bin(a | b))
+print()
+
+# ANDing
+# -----------------------------------------------------------------------------
+a = 0b1001      # 9
+b = 0b1100      # 12
+print(bin(a))
+print(bin(b))
+print(bin(a & b))
+print()
+
+# XORing (exclusive OR), that is, only 1 can be true, to return true
+# -----------------------------------------------------------------------------
+a = 0b1001      # 9
+b = 0b1100      # 12
+print(bin(a))
+print(bin(b))
+print(bin(a ^ b))
