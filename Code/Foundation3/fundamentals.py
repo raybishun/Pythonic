8951b8a8b974bf582783476b716e86d95bacb552
Ray Bishun <ray.bishun@bitsbytes.com>
2021-03-15 21:08:53 -0400

    Operators

--- Code/Foundation3/fundamentals.py
+++ Code/Foundation3/fundamentals.py
@@ -30,4 +30,27 @@ print(x)
 print(y)
 print(z)
 print(x == y)       # True
+print()
 
+# Operators
+"""
+    / is used for standard division, and ALWAYS returns a float
+    // is 'floor division', that is, only the whole number is returned
+    % modulus, returns the remainder (a common use of this is to find even/odd numbers)
+    ** are exponents 2 ** 3 = (2 x 2 x 2) = 8
+"""
+
+# Number Systems
+"""
+    Base2       Binary          0-1
+    Base8       Octal           0-7
+    Base10      Decimal         0-9
+    Base16      Hexadecimal     0-9, A-F
+"""
+
+print(f'Binary:     \t{bin(0b1001)}')
+print(f'Octal:      \t{oct(0o11)}')
+print(f'Decimal:    \t{0b1001}')
+print(f'Hexadecimal:\t{0x9}')
+
+# Converting decimal to binary
