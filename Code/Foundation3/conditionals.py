1a959fbcbf5356311583cdb7949c95cb3bdf9861
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2021-03-31 20:27:11 -0400

    Conditionals

---
+++ Code/Foundation3/conditionals.py
@@ -0,0 +1,19 @@
+# -----------------------------------------------------------------------------
+# If Statement
+# -----------------------------------------------------------------------------
+if True:
+    print('True')
+elif 'Silly1':
+    print('wtf1')
+elif 'Silly2':
+    print('wtf2')
+else:
+    print('False')
+print()
+
+
+if False:
+    print('False')
+else:
+    # print('This test will never return False...')
+    pass # a non operational statement, aka 'no op'
