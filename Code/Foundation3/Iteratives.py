d0af18a03734e39cf5e47fe1065999450f533e0e
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2021-04-02 11:58:36 -0400

    Nesting loops and conditionals

--- Code/Foundation3/Iteratives.py
+++ Code/Foundation3/Iteratives.py
@@ -1,10 +1,32 @@
 phrase = "To infinity and beyond..."
 
+# Return only even numbers using 'modulo'
 i = 1
 while i <= 10:
+    if i % 2 == 0:
+        print(f'{i}. {phrase}')
+    i += 1
+print()
+
+# Using 'continue' to return only odd numbers
+i = 1
+while i <= 10:
+    if i % 2 == 0:
+        i += 1
+        continue
+    if i == 7:
+        print(f'Found {i}, exiting...')
+        break
     print(f'{i}. {phrase}')
     i += 1
 print()
+
+
+exit()
+
+
+
+
 
 for letter in phrase:
     print(letter)
