978081f43c5e18bf0d0f0e7c7ffaa84fcc2512eb
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2021-04-02 19:48:57 -0400

    List Comprehensions

--- Code/Foundation3/Iteratives.py
+++ Code/Foundation3/Iteratives.py
@@ -40,7 +40,49 @@ else:
 
 print()
 
+# Range
+my_range = range(10)
+print(my_range)         # Returns range(0, 10)
+
+print(list(my_range))   # Convert range to a list
+
+# Stepping a range
+print(list(range(0, 110, 10)))  # Returns 0, 10, 20, ... 100
+
+# _ throwaway variable
+for _ in range(10):
+    print(f'{_}. hi...')
+
+# -----------------------------------------------------------------------------
+# List Comprehensions
+# -----------------------------------------------------------------------------
+# The normal way...
+my_nums = ['one', 'two', 'three', 'four', 'five']
+uppercase_my_nums = []
+for num in my_nums:
+    uppercase_my_nums.append(num.upper())
+print(uppercase_my_nums)
+
+# Using List Comprehensions
+uppercase_my_nums2 = [num.upper() for num in my_nums]
+print(uppercase_my_nums2)
+
+print()
+
+# Using Comprehensions for filtering
+# The normal way...
+search_results = []
+for num in my_nums:
+    if num in ['three', 'five', 'seven']:
+        search_results.append(num)
+print(search_results)
+
+# Using List Comprehensions
+search_results2 = [num for num in my_nums if num in ['three', 'five', 'seven']]
+print(search_results2)
+
 exit()
+
 
 for letter in phrase:
     print(letter)
@@ -68,4 +110,3 @@ my_dict = {1:'one', 2:'two', 3:'three'}
 for k,v in my_dict.items():
     print(k,v)
 print()
-
