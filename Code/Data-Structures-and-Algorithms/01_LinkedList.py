bfc8a2016242f0557ac65314c43ee68cf0773e4f
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2021-04-11 15:25:42 -0400

    Linked List: Insert()

--- Code/Data-Structures-and-Algorithms/01_LinkedList.py
+++ Code/Data-Structures-and-Algorithms/01_LinkedList.py
@@ -3,6 +3,24 @@ class Node:
         self.value = value
         self.next = None
 
-class LiskedList:
+class LinkedList:
     def __init__(self):
         self.start = None
+
+    def insert(self, value):
+        if self.start == None:
+            self.start = Node(value)
+
+        current = self.start
+
+        while current.next is not None:
+            current = current.next
+
+        current.next = Node(value)
+
+linked_List = LinkedList()
+linked_List.insert(1)
+linked_List.insert(2)
+linked_List.insert(3)
+linked_List.insert(4)
+linked_List.insert(5)
