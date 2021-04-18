2da45ae6add2540f982bf83e1c5ec8668fb55baa
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2021-04-17 21:35:59 -0400

    Trees

--- Code/Data-Structures-and-Algorithms/05_Trees.py
+++ Code/Data-Structures-and-Algorithms/05_Trees.py
@@ -7,15 +7,34 @@ class Node:
         self.left = None
         self.right = None
         
-    def insert(root, node):
-        if root is None:
-            root = Node
-            return root
-
-        if node.value < root.value:
-            root.left = insert(root.left, node)
-        
-        if node.value > root.value:
-            root.right = insert(root.right, node)
-        
+def insert(root, node):
+    if root is None:
+        root = node
         return root
+
+    if node.value < root.value:
+        root.left = insert(root.left, node)
+        
+    if node.value > root.value:
+        root.right = insert(root.right, node)
+        
+    return root
+
+def preorder(root):
+    if root:
+        print(root.value)
+        preorder(root.left)
+        preorder(root.right)
+
+# -----------------------------------------------------------------------------
+# Consumer
+# -----------------------------------------------------------------------------
+root = Node(50)
+insert(root, Node(10))
+insert(root, Node(2))
+insert(root, Node(80))
+insert(root, Node(15))
+insert(root, Node(60))
+insert(root, Node(90))
+
+preorder(root)
