8a31081cd379bf5e41920909afb89ca07d6fc185
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2021-04-19 19:49:50 -0400

    DS: Trees, Postorder traversal

--- Code/Data-Structures-and-Algorithms/05_Trees.py
+++ Code/Data-Structures-and-Algorithms/05_Trees.py
@@ -20,18 +20,30 @@ def insert(root, node):
         
     return root
 
+# Preorder traveral
 def preorder(root):
     if root:
         print(root.value)
         preorder(root.left)
         preorder(root.right)
 
+# Inorder traveral
 def inorder(root):
     if root:
         inorder(root.left)
         print(root.value)
         inorder(root.right)
 
+# Postorder traveral
+def postorder(root):
+    if root:
+        postorder(root.left)
+        postorder(root.right)
+        print(root.value)
+
+# -----------------------------------------------------------------------------
+# Consumer
+# -----------------------------------------------------------------------------
 # Populating a Tree
 root = Node(50)
 insert(root, Node(10))
@@ -41,9 +53,14 @@ insert(root, Node(15))
 insert(root, Node(60))
 insert(root, Node(90))
 
-# Preorder traverals of a Tree
+# Preorder traveral of a Tree
 preorder(root)
 print()
 
-# Inorder traverals of a Tree
+# Inorder traveral of a Tree
 inorder(root)
+print()
+
+# Postorder traveral of a Tree
+postorder(root)
+print()
