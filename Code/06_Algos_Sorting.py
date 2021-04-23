80ced0cf666361f1cf2850000186a725ae10e461
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2021-04-23 19:39:57 -0400

    Algos: Sorting: Selection Sort

--- Code/06_Algos_Sorting.py
+++ Code/06_Algos_Sorting.py
@@ -11,9 +11,28 @@ def insertionSort(list):
     
         list[j] = value
 
-# -----------------------------------------------------------------------------
-# Consumer
-# -----------------------------------------------------------------------------
+# Insertion Sort Consumer
 list = [30, 44, 67, 5, 8, 9, 53]
 insertionSort(list)
 print(list)
+print()
+
+# -----------------------------------------------------------------------------
+# Algorithms: Sorting: Selection Sort
+# -----------------------------------------------------------------------------
+def selectionSort(list):
+    for i in range(0, len(list) -1):
+        minValueIndex = i
+        for j in range(i + 1, len(list)):
+            if(list[minValueIndex] > list[j]):
+                minValueIndex = j
+
+        tempValue = list[i]
+        list[i] = list[minValueIndex]
+        list[minValueIndex] = tempValue
+
+# Selection Sort Consumer
+list = [30, 44, 67, 5, 8, 9, 53]
+selectionSort(list)
+print(list)
+print()
